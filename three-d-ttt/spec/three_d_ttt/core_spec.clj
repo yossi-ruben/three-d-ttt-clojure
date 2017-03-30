c520ac5112583fe4ae4a6f4b388c493d8a001453
Yossi Ruben <yossimruben@gmail.com>
2017-03-30 15:46:01 -0400

    bit late to start git

---
+++ three-d-ttt/spec/three_d_ttt/core_spec.clj
@@ -0,0 +1,88 @@
+(ns three-d-ttt.core-spec
+  (:require [speclj.core :refer :all]
+            [three-d-ttt.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 0)))
+
+
+
+(describe "the board"
+	(it "should be vec 1-9"
+		(should= board [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26])))
+
+
+(describe "valid-move?"
+	(it "should be true open inbound space"
+		(should= true (valid-move? board 2)))
+
+	(it "catched out of bounds"
+		(should= false (valid-move? board 88)))
+
+	(it "returns false for bad input"
+		(should= false (valid-move? board "a")))
+
+	(it "returns false if place is taken"
+		(should-be-nil (valid-move? ["x" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26] 0))))
+
+
+
+(describe "place-piece"
+	(it "should place piece"
+		(should= ["x" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26] (place-piece board "x" 0))))
+
+
+
+(describe "selects opposing piece"	
+	(it "should return opposing piece"
+		(should= "x" (opposing-piece "0"))))
+
+
+
+(describe "return new board for turn")
+
+
+
+(describe "winnin-board"
+	(it "should be 1"
+		(should= 1 (winning-board? ["x" "x" "x" 3 4 5 6 7 8]))))
+
+
+(describe "checks all slices for win"
+	(it "should return 2 if no win"
+		(should= 2 (check-all-slices-for-win? board)))
+
+	(it "should return 1 if win on diagonal slice 0 13 26"
+		(should= 1 (check-all-slices-for-win? ["x" 1 2 3 4 5 6 7 8 9 10 11 12 "x" 14 15 16 17 18 19 20 21 22 23 24 25 "x"])))
+
+	(it "should return 1 if win on diagonal slice 18 22 26"
+		(should= 1 (check-all-slices-for-win? [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 "x" 19 20 21 "x" 23 24 25 "x"])))
+
+)
+
+
+; (describe "game-over"
+; 	(it "should not be a game over"
+; 		(should= false (game-over? board))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
---
+++ three-d-ttt/src/three_d_ttt/core.clj
@@ -0,0 +1,87 @@
+(ns three-d-ttt.core)
+
+
+(def board [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26])
+
+
+(defn print-board [board]
+	(println)
+ 	(println (str (board 0) " | " (board 1) " | " (board 2) ) )
+  	(println "-------------")
+  	(println (str (board 3) " | " (board 4) " | " (board 5) ) )
+  	(println "-------------")
+  	(println (str (board 6) " | " (board 7) " | " (board 8) ) )
+  	(println))
+
+
+(defn slice-and-print [board]
+	(print-board (subvec board 18 27))
+	(print-board (subvec board 9 18))
+	(print-board (subvec board 0 9)))
+
+
+(defn valid-move? [board place]
+ 	(if (and (try (Integer. place) (catch Exception e ))  (and (>= (Integer. place) 0) (< (Integer. place) (count board))) )
+        (if (= java.lang.Long (class (board (Integer. place)))) true) false))
+
+
+(defn opposing-piece [piece]
+ 	(if (= piece "x") "o" "x"))
+
+
+(defn place-piece[board piece place]
+ 	(assoc board (Integer. place) piece))
+
+
+(defn take-turn [board piece]
+  	(slice-and-print board)
+  	(println (str piece " - Its your turn"))
+  	(println "Select Slot (0 - 26) ")
+  	(let [place (read-line) board board]
+  		(if (valid-move? board place)
+  	  	(place-piece board piece place)
+  	  	(take-turn board piece))))
+
+
+(defn winning-board? [board]
+	(cond 
+	  (some true? (map #(apply = %) (partition-all 3 board))) 1
+	  (some true? (map #(apply = %) (take 3 (apply mapv list (partition 3 board))))) 1
+	  (apply = (take-nth 4 board)) 1
+	  (apply = (take 3 (take-nth 2 (drop 2 board)))) 1
+	  :else 2))
+
+
+(defn check-all-slices-for-win? [board]
+	(cond
+		;checks board 1 2 3
+		(some #(= 1 %) (map #(winning-board? %) (map #(into [] %) (partition 9 board)))) 1
+		;check board 4 5 6
+		(some #(= 1 %) (map #(winning-board? %) (apply mapv vector (partition 3 board)))) 1
+		;check board 7 8 9
+		(some #(= 1 %) (map #(winning-board? %)  (map #(into [] %)(partition 9 (flatten (partition 3 (apply mapv vector (partition 9 board)))))))) 1
+		;check board 10 
+		(some #(= 1 %)(map #(winning-board? %) (vector (into [] (map #(get board %) [0 10 20 3 13 23 6 16 26]))))) 1
+		;check board 11
+		(some #(= 1 %)(map #(winning-board? %) (vector (into [] (map #(get board %) [18 10 2 21 13 5 24 16 8]))))) 1
+		:else 2))
+
+
+(defn game-over? [board]
+ 	(if (or (= 1 (check-all-slices-for-win? board)) (not-any? number? board) ) true false))
+
+
+(defn play-game [board sym]
+  	(if (game-over? board) 
+  		(do 
+  	  	(if (= 1 (check-all-slices-for-win?  board) )	
+  		(println (str "Great game! " (opposing-piece sym)))
+  		(println "Its a TIE")))
+		(play-game (take-turn board sym) (opposing-piece sym))))
+
+; (play-game board "x")
+
+
+(defn -main
+  [& args]
+  (println "Hello World"))
